edd0fd534d3ba2b968d3a18ed4d3597d961d93af
github-action-benchmark <github@users.noreply.github.com>
2025-09-30 04:32:01 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 8488cb0c4fe6669e101d0aa94c13b5ef80c892bb

---
+++ dev/bench/data.js
@@ -0,0 +1,52 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1759206721693,
+  "repoUrl": "https://github.com/WisdomRoot/pqcim",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "banacorn@gmail.com",
+            "name": "Ting-Gian LUA",
+            "username": "banacorn"
+          },
+          "committer": {
+            "email": "banacorn@gmail.com",
+            "name": "Ting-Gian LUA",
+            "username": "banacorn"
+          },
+          "distinct": true,
+          "id": "8488cb0c4fe6669e101d0aa94c13b5ef80c892bb",
+          "message": "[ change ] Use \"+\" prefix for flags in the CLI to workaround the limitation of Makefile",
+          "timestamp": "2025-09-30T12:19:11+08:00",
+          "tree_id": "d3d4ce15a7a1514c22ed6b57bc9e2d264a003707",
+          "url": "https://github.com/WisdomRoot/pqcim/commit/8488cb0c4fe6669e101d0aa94c13b5ef80c892bb"
+        },
+        "date": 1759206721414,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "segmented_lut_modular_multiplier_WNS",
+            "value": 8.999,
+            "unit": "ns"
+          },
+          {
+            "name": "ntt_input_adapter_WNS",
+            "value": 9.0913,
+            "unit": "ns"
+          },
+          {
+            "name": "cfg_adapter_WNS",
+            "value": 9.2492,
+            "unit": "ns"
+          },
+          {
+            "name": "axi_stream_skid_buffer_WNS",
+            "value": 9.6729,
+            "unit": "ns"
+          }
+        ]
+      }
+    ]
+  }
+}
