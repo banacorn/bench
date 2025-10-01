67296c31146f98f4d0e73c6232aaaba2f5e82265
github-action-benchmark <github@users.noreply.github.com>
2025-10-01 07:32:42 +0000

    add Benchmark (customBiggerIsBetter) benchmark result for 7d812c7ead2688250995ffa4484b411b71e60152

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1759226081397,
+  "lastUpdate": 1759303962423,
   "repoUrl": "https://github.com/WisdomRoot/pqcim",
   "entries": {
     "Benchmark": [
@@ -69,6 +69,55 @@ window.BENCHMARK_DATA = {
         "date": 1759226081105,
         "tool": "customBiggerIsBetter",
         "benches": [
+          {
+            "name": "segmented_lut_modular_multiplier_WNS",
+            "value": 8.999,
+            "unit": "ns"
+          },
+          {
+            "name": "ntt_input_adapter_WNS",
+            "value": 9.0913,
+            "unit": "ns"
+          },
+          {
+            "name": "cfg_adapter_WNS",
+            "value": 9.2492,
+            "unit": "ns"
+          },
+          {
+            "name": "axi_stream_skid_buffer_WNS",
+            "value": 9.6729,
+            "unit": "ns"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "banacorn@gmail.com",
+            "name": "Ting-Gian LUA",
+            "username": "banacorn"
+          },
+          "committer": {
+            "email": "banacorn@gmail.com",
+            "name": "Ting-Gian LUA",
+            "username": "banacorn"
+          },
+          "distinct": true,
+          "id": "7d812c7ead2688250995ffa4484b411b71e60152",
+          "message": "[ fix ] Failing tests of the CLI itself",
+          "timestamp": "2025-10-01T14:53:22+08:00",
+          "tree_id": "586f129bcf06f0fa6f82a33bf3806f0c9b563406",
+          "url": "https://github.com/WisdomRoot/pqcim/commit/7d812c7ead2688250995ffa4484b411b71e60152"
+        },
+        "date": 1759303961877,
+        "tool": "customBiggerIsBetter",
+        "benches": [
+          {
+            "name": "modular_arithmetic_element_WNS",
+            "value": 8.7792,
+            "unit": "ns"
+          },
           {
             "name": "segmented_lut_modular_multiplier_WNS",
             "value": 8.999,
